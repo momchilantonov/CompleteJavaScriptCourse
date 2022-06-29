32feb1b1b08b45fb28b2b72f7a4585b04f7f1e26
momchilantonov <eng.antonov@gmail.com>
2022-06-29 13:01:27 +0300

    Operator Precedence

--- 01-Fundamentals-Part-1/lectures.js
+++ 01-Fundamentals-Part-1/lectures.js
@@ -78,6 +78,7 @@
 // const lastName = 'Schmedtmann';
 // console.log(firstName + ' ' + lastName);
 
+//////////Basic Operators//////////
 //////////Assignment operators//////////
 // let x = 10 + 5; // 15
 // x += 10; // x = x + 10 = 25
@@ -87,6 +88,7 @@
 // x--;
 // console.log(x);
 
+//////////Basic Operators//////////
 //////////Comparison operators//////////
 // console.log(ageJonas > ageSarah); // >, <, >=, <=
 // console.log(ageSarah >= 18);
@@ -94,3 +96,17 @@
 // const isFullAge = ageSarah >= 18;
 
 // console.log(now - 1991 > now - 2018);
+
+//////////Operator Precedence//////////
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+
+// console.log(now - 1991 > now - 2018);
+
+// let x, y;
+// x = y = 25 - 10 - 5; // x = y = 10, x = 10
+// console.log(x, y);
+
+// const averageAge = (ageJonas + ageSarah) / 2;
+// console.log(ageJonas, ageSarah, averageAge);
