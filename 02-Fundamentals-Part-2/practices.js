02ec5c31e5db22df4eadf5f6fed9c71fb5530b00
momchilantonov <eng.antonov@gmail.com>
2022-07-05 11:59:34 +0300

    Functions

---
+++ 02-Fundamentals-Part-2/practices.js
@@ -0,0 +1,8 @@
+//////////Functions//////////
+function describeCountry(country, population, capitalCity) {
+    return `${country} has ${population} million people and its capital city is ${capitalCity}`;
+}
+
+console.log(describeCountry("Bulgaria", 6, "Sofia"));
+console.log(describeCountry("Italy", 15, "Rome"));
+console.log(describeCountry("Russia", 50, "Moscow"));
