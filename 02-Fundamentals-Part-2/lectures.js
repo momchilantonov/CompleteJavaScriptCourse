ea9bbd1623dff14af21530c4677b483d1d82406b
momchilantonov <eng.antonov@gmail.com>
2022-07-05 12:24:22 +0300

    Function Declarations & Expressions

--- 02-Fundamentals-Part-2/lectures.js
+++ 02-Fundamentals-Part-2/lectures.js
@@ -30,3 +30,18 @@
   
 //   const appleOrangeJuice = fruitProcessor(2, 4);
 //   console.log(appleOrangeJuice);
+
+//////////Function Declarations vs. Expressions//////////
+// // Function declaration
+// function calcAge1(birthYeah) {
+//     return 2037 - birthYeah;
+//   }
+//   const age1 = calcAge1(1991);
+  
+//   // Function expression
+//   const calcAge2 = function (birthYeah) {
+//     return 2037 - birthYeah;
+//   }
+//   const age2 = calcAge2(1991);
+  
+//   console.log(age1, age2);
--- 02-Fundamentals-Part-2/practices.js
+++ 02-Fundamentals-Part-2/practices.js
@@ -6,3 +6,34 @@ function describeCountry(country, population, capitalCity) {
 console.log(describeCountry("Bulgaria", 6, "Sofia"));
 console.log(describeCountry("Italy", 15, "Rome"));
 console.log(describeCountry("Russia", 50, "Moscow"));
+
+//////////Function Declarations vs. Expressions//////////
+function percentageOfWorld1(population) {
+    const worldPopulation = 7900;
+    const percentagePopulation = (population / worldPopulation) * 100;
+
+    return percentagePopulation
+}
+
+const bulgariaPopulationPrcnt1 = percentageOfWorld1(6);
+const italyPopulationPrcnt1 = percentageOfWorld1(15);
+const russiaPopulationPrcnt1 = percentageOfWorld1(50);
+
+console.log(bulgariaPopulationPrcnt1);
+console.log(italyPopulationPrcnt1);
+console.log(russiaPopulationPrcnt1);
+
+const percentageOfWorld2 = function (population) {
+    const worldPopulation = 7900;
+    const percentagePopulation = (population / worldPopulation) * 100;
+
+    return percentagePopulation
+}
+
+const bulgariaPopulationPrcnt2 = percentageOfWorld2(6);
+const italyPopulationPrcnt2 = percentageOfWorld2(15);
+const russiaPopulationPrcnt2 = percentageOfWorld2(50);
+
+console.log(bulgariaPopulationPrcnt2);
+console.log(italyPopulationPrcnt2);
+console.log(russiaPopulationPrcnt2);
