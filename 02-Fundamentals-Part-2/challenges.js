ecb7ff4cf4d5a11db7e89eb9f2a1b9cddd9df394
momchilantonov <eng.antonov@gmail.com>
2022-09-15 14:24:49 +0300

    Fundamentals Part 1 - Challenge 2

--- 02-Fundamentals-Part-2/challenges.js
+++ 02-Fundamentals-Part-2/challenges.js
@@ -1,35 +1,67 @@
-//////////Coding Challenge #1//////////
+// //////////Coding Challenge #1//////////
+// // Task1
+// const calcAverage = scores => scores.reduce((a, b) => a + b, 0) / scores.length;
+
+// // Task2
+// const avgDolphins1 = calcAverage([44, 23, 71]);
+// const avgDolphins2 = calcAverage([85, 54, 41]);
+// const avgKoalas1 = calcAverage([65, 54, 49]);
+// const avgKoalas2 = calcAverage([23, 34, 27]);
+
+// console.log(avgDolphins1);
+// console.log(avgDolphins2);
+// console.log(avgKoalas1);
+// console.log(avgKoalas2);
+
+// // Task3
+// function checkWinner(avgScores) {
+//     const avgHome = avgScores[0];
+//     const avgGuest = avgScores[1];
+//     const homeName = "Dolphins";
+//     const guestName = "Koalas";
+
+
+//     if (avgHome >= avgGuest * 2) {
+//         return `${homeName} win (${avgHome} vs. ${avgGuest})`;
+//     } else if (avgGuest >= avgHome * 2) {
+//         return`${guestName} win (${avgGuest} vs. ${avgHome})`;
+//     } else {
+//         return "No winner";
+//     }
+// }
+
+// // Task4
+// console.log(checkWinner([avgDolphins1, avgKoalas1]));
+// console.log(checkWinner([avgDolphins2, avgKoalas2]));
+
+//////////Coding Challenge #2//////////
 // Task1
-const calcAverage = scores => scores.reduce((a, b) => a + b, 0) / scores.length;
+function calcTip(billVal) {
+    const tipVal = billVal >= 50 && billVal <= 300 ? billVal * 0.15 : billVal * 0.2;
 
-// Task2
-const avgDolphins1 = calcAverage([44, 23, 71]);
-const avgDolphins2 = calcAverage([85, 54, 41]);
-const avgKoalas1 = calcAverage([65, 54, 49]);
-const avgKoalas2 = calcAverage([23, 34, 27]);
-
-console.log(avgDolphins1);
-console.log(avgDolphins2);
-console.log(avgKoalas1);
-console.log(avgKoalas2);
-
-// Task3
-function checkWinner(avgScores) {
-    const avgHome = avgScores[0];
-    const avgGuest = avgScores[1];
-    const homeName = "Dolphins";
-    const guestName = "Koalas";
-
-
-    if (avgHome >= avgGuest * 2) {
-        return `${homeName} win (${avgHome} vs. ${avgGuest})`;
-    } else if (avgGuest >= avgHome * 2) {
-        return`${guestName} win (${avgGuest} vs. ${avgHome})`;
-    } else {
-        return "No winner";
-    }
+    return tipVal;
 }
 
+console.log(calcTip(100));
+
+// Task2
+const bills = [125, 555, 44];
+
+// Task3
+const tips = [
+    calcTip(bills[0]),
+    calcTip(bills[1]),
+    calcTip(bills[2]),
+];
+
+console.log(tips);
+
 // Task4
-console.log(checkWinner([avgDolphins1, avgKoalas1]));
-console.log(checkWinner([avgDolphins2, avgKoalas2]));
+const total = [];
+
+for (let i = 0; i < bills.length; i++) {
+    let cuurentBill = bills[i] + bills[i] * calcTip(bills[i])
+    total.push(cuurentBill);
+}
+
+console.log(total);
