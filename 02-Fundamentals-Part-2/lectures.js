2cd465c4f36ada4b49f00ccc35a44e598b272718
momchilantonov <eng.antonov@gmail.com>
2022-09-15 14:32:34 +0300

    Introductions to Objects

--- 02-Fundamentals-Part-2/lectures.js
+++ 02-Fundamentals-Part-2/lectures.js
@@ -165,3 +165,20 @@
 // if (friends.includes('Steven')) {
 //   console.log('You have a friend called Steven');
 // }
+
+//////////Introduction to Objects//////////
+// const jonasArray = [
+//     'Jonas',
+//     'Schmedtmann',
+//     2037 - 1991,
+//     'teacher',
+//     ['Michael', 'Peter', 'Steven']
+//   ];
+  
+//   const jonas = {
+//     firstName: 'Jonas',
+//     lastName: 'Schmedtmann',
+//     age: 2037 - 1991,
+//     job: 'teacher',
+//     friends: ['Michael', 'Peter', 'Steven']
+//   };
