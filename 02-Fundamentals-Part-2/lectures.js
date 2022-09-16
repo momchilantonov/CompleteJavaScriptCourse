20282a3941cbd558501942a4494523a5949c2468
momchilantonov <eng.antonov@gmail.com>
2022-09-16 14:10:27 +0300

    For loop - Lecture & Practice

--- 02-Fundamentals-Part-2/lectures.js
+++ 02-Fundamentals-Part-2/lectures.js
@@ -256,3 +256,20 @@
 //   // Challenge
 //   // "Jonas is a 46-year old teacher, and he has a driver's license"
 //   console.log(jonas.getSummary());
+
+//////////Iteration: The for Loop//////////
+// // console.log('Lifting weights repetition 1 🏋️‍♀️');
+// // console.log('Lifting weights repetition 2 🏋️‍♀️');
+// // console.log('Lifting weights repetition 3 🏋️‍♀️');
+// // console.log('Lifting weights repetition 4 🏋️‍♀️');
+// // console.log('Lifting weights repetition 5 🏋️‍♀️');
+// // console.log('Lifting weights repetition 6 🏋️‍♀️');
+// // console.log('Lifting weights repetition 7 🏋️‍♀️');
+// // console.log('Lifting weights repetition 8 🏋️‍♀️');
+// // console.log('Lifting weights repetition 9 🏋️‍♀️');
+// // console.log('Lifting weights repetition 10 🏋️‍♀️');
+
+// // for loop keeps running while condition is TRUE
+// for (let rep = 1; rep <= 30; rep++) {
+//     console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
+//   }
--- 02-Fundamentals-Part-2/practices.js
+++ 02-Fundamentals-Part-2/practices.js
@@ -1,100 +1,100 @@
-// //////////Functions//////////
-// function describeCountry(country, population, capitalCity) {
-//     return `${country} has ${population} million people and its capital city is ${capitalCity}`;
-// }
+//////////Functions//////////
+function describeCountry(country, population, capitalCity) {
+    return `${country} has ${population} million people and its capital city is ${capitalCity}`;
+}
 
-// console.log(describeCountry("Bulgaria", 6, "Sofia"));
-// console.log(describeCountry("Italy", 15, "Rome"));
-// console.log(describeCountry("Russia", 50, "Moscow"));
+console.log(describeCountry("Bulgaria", 6, "Sofia"));
+console.log(describeCountry("Italy", 15, "Rome"));
+console.log(describeCountry("Russia", 50, "Moscow"));
 
-// //////////Function Declarations vs. Expressions//////////
-// function percentageOfWorld1(population) {
-//     const worldPopulation = 7900;
-//     const percentagePopulation = (population / worldPopulation) * 100;
+//////////Function Declarations vs. Expressions//////////
+function percentageOfWorld1(population) {
+    const worldPopulation = 7900;
+    const percentagePopulation = (population / worldPopulation) * 100;
 
-//     return percentagePopulation
-// }
+    return percentagePopulation
+}
 
-// const bulgariaPopulationPrcnt1 = percentageOfWorld1(6);
-// const italyPopulationPrcnt1 = percentageOfWorld1(15);
-// const russiaPopulationPrcnt1 = percentageOfWorld1(50);
+const bulgariaPopulationPrcnt1 = percentageOfWorld1(6);
+const italyPopulationPrcnt1 = percentageOfWorld1(15);
+const russiaPopulationPrcnt1 = percentageOfWorld1(50);
 
-// console.log(bulgariaPopulationPrcnt1);
-// console.log(italyPopulationPrcnt1);
-// console.log(russiaPopulationPrcnt1);
+console.log(bulgariaPopulationPrcnt1);
+console.log(italyPopulationPrcnt1);
+console.log(russiaPopulationPrcnt1);
 
-// const percentageOfWorld2 = function (population) {
-//     const worldPopulation = 7900;
-//     const percentagePopulation = (population / worldPopulation) * 100;
+const percentageOfWorld2 = function (population) {
+    const worldPopulation = 7900;
+    const percentagePopulation = (population / worldPopulation) * 100;
 
-//     return percentagePopulation
-// }
+    return percentagePopulation
+}
 
-// const bulgariaPopulationPrcnt2 = percentageOfWorld2(6);
-// const italyPopulationPrcnt2 = percentageOfWorld2(15);
-// const russiaPopulationPrcnt2 = percentageOfWorld2(50);
+const bulgariaPopulationPrcnt2 = percentageOfWorld2(6);
+const italyPopulationPrcnt2 = percentageOfWorld2(15);
+const russiaPopulationPrcnt2 = percentageOfWorld2(50);
 
-// console.log(bulgariaPopulationPrcnt2);
-// console.log(italyPopulationPrcnt2);
-// console.log(russiaPopulationPrcnt2);
+console.log(bulgariaPopulationPrcnt2);
+console.log(italyPopulationPrcnt2);
+console.log(russiaPopulationPrcnt2);
 
-// //////////Arrow Functions//////////
-// const percentageOfWorld3 = population => (population / 7900) * 100;
+//////////Arrow Functions//////////
+const percentageOfWorld3 = population => (population / 7900) * 100;
 
-// const bulgariaPopulationPrcnt3 = percentageOfWorld3(6);
-// const italyPopulationPrcnt3 = percentageOfWorld3(15);
-// const russiaPopulationPrcnt3 = percentageOfWorld3(50);
+const bulgariaPopulationPrcnt3 = percentageOfWorld3(6);
+const italyPopulationPrcnt3 = percentageOfWorld3(15);
+const russiaPopulationPrcnt3 = percentageOfWorld3(50);
 
-// console.log(bulgariaPopulationPrcnt3);
-// console.log(italyPopulationPrcnt3);
-// console.log(russiaPopulationPrcnt3);
+console.log(bulgariaPopulationPrcnt3);
+console.log(italyPopulationPrcnt3);
+console.log(russiaPopulationPrcnt3);
 
-// //////////Functions Calling Other Functions//////////
-// function describePopulation(country, population) {
-//     const populationPrcnt = percentageOfWorld1(population)
-//     return `${country} has ${population} million people, which is about ${populationPrcnt.toFixed(2)}% of the world.`
-// }
+//////////Functions Calling Other Functions//////////
+function describePopulation(country, population) {
+    const populationPrcnt = percentageOfWorld1(population)
+    return `${country} has ${population} million people, which is about ${populationPrcnt.toFixed(2)}% of the world.`
+}
 
-// const bulgariaPopulationDescribe = describePopulation("Bulgaria", 6);
-// const italyPopulationDescribe = describePopulation("Italy", 15);
-// const russiaPopulationDescribe = describePopulation("China", 1441);
+const bulgariaPopulationDescribe = describePopulation("Bulgaria", 6);
+const italyPopulationDescribe = describePopulation("Italy", 15);
+const russiaPopulationDescribe = describePopulation("China", 1441);
 
-// console.log(bulgariaPopulationDescribe);
-// console.log(italyPopulationDescribe);
-// console.log(russiaPopulationDescribe);
+console.log(bulgariaPopulationDescribe);
+console.log(italyPopulationDescribe);
+console.log(russiaPopulationDescribe);
 
-// //////////Introduction to Arrays//////////
-// const population = [6, 15, 137, 1441];
+//////////Introduction to Arrays//////////
+const population = [6, 15, 137, 1441];
 
-// console.log(population.length === 4);
+console.log(population.length === 4);
 
-// const percentages = [
-//     percentageOfWorld1(population[0]),
-//     percentageOfWorld1(population[1]),
-//     percentageOfWorld1(population[2]),
-//     percentageOfWorld1(population[3]),
-// ]
+const percentages = [
+    percentageOfWorld1(population[0]),
+    percentageOfWorld1(population[1]),
+    percentageOfWorld1(population[2]),
+    percentageOfWorld1(population[3]),
+]
 
-// console.log(percentages)
+console.log(percentages)
 
-// //////////Basic Array Operations (Methods)//////////
-// const neighbours = ["Greece", "Turkey", "Serbia"];
+//////////Basic Array Operations (Methods)//////////
+const neighbours = ["Greece", "Turkey", "Serbia"];
 
-// neighbours.push("Utopia");
+neighbours.push("Utopia");
 
-// console.log(neighbours);
+console.log(neighbours);
 
-// neighbours.pop();
+neighbours.pop();
 
-// console.log(neighbours);
+console.log(neighbours);
 
-// if (!neighbours.includes("Germani")) {
-//     console.log("Probably not a central European country :D");
-// }
+if (!neighbours.includes("Germani")) {
+    console.log("Probably not a central European country :D");
+}
 
-// neighbours[neighbours.indexOf("Serbia")] = "Republic of Serbia";
+neighbours[neighbours.indexOf("Serbia")] = "Republic of Serbia";
 
-// console.log(neighbours);
+console.log(neighbours);
 
 //////////Introduction to Objects && Object Methods//////////
 const myCountry = {
@@ -117,6 +117,13 @@ console.log(myCountry.describe());
 console.log(myCountry.checkIsIsland());
 console.log(myCountry.isIsland);
 
-//////////Dot vs. Bracket Notation//////////
-// console.log(`${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people,
-// ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}.`)
+////////Dot vs. Bracket Notation//////////
+console.log(`${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people,
+${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}.`)
+
+//////////Iteration: The for Loop//////////
+const votersNumber = 50;
+
+for (let i = 1; i <= votersNumber; i++) {
+    console.log(`Voter number ${i} is currently voting`);
+}
