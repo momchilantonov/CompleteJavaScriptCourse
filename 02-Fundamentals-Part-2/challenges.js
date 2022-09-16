bfa02fe608c78b07bbdd5df28bea93ffb5bd45ae
momchilantonov <eng.antonov@gmail.com>
2022-09-16 13:55:58 +0300

    FUndamentals Part 2 - Chellenge 3

--- 02-Fundamentals-Part-2/challenges.js
+++ 02-Fundamentals-Part-2/challenges.js
@@ -1,38 +1,38 @@
-// //////////Coding Challenge #1//////////
-// // Task1
-// const calcAverage = scores => scores.reduce((a, b) => a + b, 0) / scores.length;
+//////////Coding Challenge #1//////////
+// Task1
+const calcAverage = scores => scores.reduce((a, b) => a + b, 0) / scores.length;
 
-// // Task2
-// const avgDolphins1 = calcAverage([44, 23, 71]);
-// const avgDolphins2 = calcAverage([85, 54, 41]);
-// const avgKoalas1 = calcAverage([65, 54, 49]);
-// const avgKoalas2 = calcAverage([23, 34, 27]);
+// Task2
+const avgDolphins1 = calcAverage([44, 23, 71]);
+const avgDolphins2 = calcAverage([85, 54, 41]);
+const avgKoalas1 = calcAverage([65, 54, 49]);
+const avgKoalas2 = calcAverage([23, 34, 27]);
 
-// console.log(avgDolphins1);
-// console.log(avgDolphins2);
-// console.log(avgKoalas1);
-// console.log(avgKoalas2);
+console.log(avgDolphins1);
+console.log(avgDolphins2);
+console.log(avgKoalas1);
+console.log(avgKoalas2);
 
-// // Task3
-// function checkWinner(avgScores) {
-//     const avgHome = avgScores[0];
-//     const avgGuest = avgScores[1];
-//     const homeName = "Dolphins";
-//     const guestName = "Koalas";
+// Task3
+function checkWinner(avgScores) {
+    const avgHome = avgScores[0];
+    const avgGuest = avgScores[1];
+    const homeName = "Dolphins";
+    const guestName = "Koalas";
 
 
-//     if (avgHome >= avgGuest * 2) {
-//         return `${homeName} win (${avgHome} vs. ${avgGuest})`;
-//     } else if (avgGuest >= avgHome * 2) {
-//         return`${guestName} win (${avgGuest} vs. ${avgHome})`;
-//     } else {
-//         return "No winner";
-//     }
-// }
+    if (avgHome >= avgGuest * 2) {
+        return `${homeName} win (${avgHome} vs. ${avgGuest})`;
+    } else if (avgGuest >= avgHome * 2) {
+        return`${guestName} win (${avgGuest} vs. ${avgHome})`;
+    } else {
+        return "No winner";
+    }
+}
 
-// // Task4
-// console.log(checkWinner([avgDolphins1, avgKoalas1]));
-// console.log(checkWinner([avgDolphins2, avgKoalas2]));
+// Task4
+console.log(checkWinner([avgDolphins1, avgKoalas1]));
+console.log(checkWinner([avgDolphins2, avgKoalas2]));
 
 //////////Coding Challenge #2//////////
 // Task1
@@ -65,3 +65,31 @@ for (let i = 0; i < bills.length; i++) {
 }
 
 console.log(total);
+
+//////////Coding Challenge #3//////////
+// Task1
+const markBMI = {
+    fullname: "Mark Miller",
+    weight: 78,
+    hight: 1.69,
+    calcBMI: function() {
+        this.bmi = this.weight / this.hight ** 2;
+        
+        return this.bmi
+    }
+};
+
+const johnBMI = {
+    fullname: "John Smith",
+    weight: 92,
+    hight: 1.95,
+    calcBMI: function() {
+        this.bmi = this.weight / this.hight ** 2;
+        
+        return this.bmi
+    }
+};
+
+console.log(
+    `${markBMI.calcBMI() < johnBMI.calcBMI() ? `Mark's BMI (${markBMI.bmi.toFixed(2)}) is higher than John's (${jhonBMI.bmi.toFixed(2)})!`: `John's BMI (${johnBMI.bmi.toFixed(2)}) is higher than Mark's (${markBMI.bmi.toFixed(2)})!`}`
+)
